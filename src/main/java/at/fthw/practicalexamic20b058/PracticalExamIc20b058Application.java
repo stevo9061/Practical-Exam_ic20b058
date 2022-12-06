79472d2d808c0c3dfe9111bbe2030cdd09d6a490
stevo9061 <sbittgen1@gmail.com>
2022-12-06 15:19:46 +0100

    first commit

---
+++ src/main/java/at/fthw/practicalexamic20b058/PracticalExamIc20b058Application.java
@@ -0,0 +1,13 @@
+package at.fthw.practicalexamic20b058;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticalExamIc20b058Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticalExamIc20b058Application.class, args);
+    }
+
+}
---
+++ src/test/java/at/fthw/practicalexamic20b058/PracticalExamIc20b058ApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fthw.practicalexamic20b058;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticalExamIc20b058ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
